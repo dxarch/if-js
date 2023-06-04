1dff6d3269587eb77ac274f34939517a21179bd7
dxarch <emilepatch@gmail.com>
2023-06-04 21:02:08 +0300

    Finish lesson 4

---
+++ tests/functions.test.js
@@ -0,0 +1,12 @@
+import {sum, getColor, colors} from "../scripts/functions";
+test('Two plus five equals seven', () => {
+    expect(sum(2)(5)).toBe(7);
+});
+
+test('Two plus five equals seven', () => {
+    expect(sum(2)(5)).toBe(7);
+});
+
+test('Tenth click returns skyblue color form array', () => {
+    expect(getColor(colors, 10)).toBe('skyblue');
+});
