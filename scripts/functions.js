1cd978849f89e34694132bd83b4506d8b1bb0c70
dxarch <emilepatch@gmail.com>
2023-06-17 20:52:46 +0300

    Add jest tests

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,13 +1,60 @@
 const parseDate = (date) => {
   const regexp = /\b(\d{2,4})-(0?[1-9]|\d{2})-(0?[1-9]|\d{2})\b/;
+  if (!regexp.test(date)) {
+    throw new Error('Input date doesn\'t match format!');
+  }
+
   return date.replace(regexp, '$3.$2.$1');
 };
 
-const findDataByQuery = (inputData, query) => {
+const data = [
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold',
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine',
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship',
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA',
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel',
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte',
+  },
+];
+
+const findDataByQuery = (query) => {
   const outData = [];
 
-  for (let i = 0; i < inputData.length; i++) {
-    const objValues = Object.values(inputData[i]);
+  for (let i = 0; i < data.length; i++) {
+    const objValues = Object.values(data[i]);
 
     for (let j = 0; j < objValues.length; j++) {
       if (objValues[j].toLowerCase().includes(query.toLowerCase())) {
--- tests/functions.test.js
+++ tests/functions.test.js
@@ -1,7 +1,18 @@
-import { sum, getColor, colors } from '../scripts/functions';
-test('Two plus five equals seven', () => {
-  expect(sum(2)(5)).toBe(7);
+import { parseDate, findDataByQuery } from '../scripts/functions';
+test('2020-11-26 converts to 26.11.2020', () => {
+  expect(parseDate('2020-11-26')).toBe('26.11.2020');
 });
-test('Tenth click returns skyblue color form array', () => {
-  expect(getColor(colors, 10)).toBe('skyblue');
+
+test('Parse date throws an error in case of wrong input', () => {
+  expect(() => parseDate('2020.11.26')).toThrow('Input date doesn\'t match format!');
+});
+
+test('"Hotel" query returns ["Russia", "Saint-Petersburg, "Hotel Leopold", "Marocco", "Ourika", "Rokoko Hotel", ' +
+    '"Germany", "Berlin", "Hotel Rehberge Berlin Mitte"]', () => {
+  expect(findDataByQuery('Hotel')).toContain("Russia", "Saint-Petersburg",
+      "Hotel Leopold", "Marocco", "Ourika", "Rokoko Hotel","Germany", "Berlin", "Hotel Rehberge Berlin Mitte");
+});
+
+test('When entering a request for which there is no data, empty array is returned', () => {
+  expect(findDataByQuery('Hello!')).toHaveLength(0);
 });
