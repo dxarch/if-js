d4ebaea2d6127ad4df3227b0ec611585d90d66c4
dxarch <emilepatch@gmail.com>
2023-06-29 00:19:14 +0300

    Add unit tests

--- tests/functions.test.js
+++ tests/functions.test.js
@@ -4,14 +4,28 @@ test('2020-11-26 converts to 26.11.2020', () => {
 });
 
 test('Parse date throws an error in case of wrong input', () => {
-  expect(() => parseDate('2020.11.26')).toThrow('Input date doesn\'t match format!');
+  expect(() => parseDate('2020.11.26')).toThrow(
+    "Input date doesn't match format!",
+  );
 });
 
-test('"Hotel" query returns ["Russia", "Saint-Petersburg, "Hotel Leopold", "Marocco", "Ourika", "Rokoko Hotel", ' +
-    '"Germany", "Berlin", "Hotel Rehberge Berlin Mitte"]', () => {
-  expect(findDataByQuery('Hotel')).toContain("Russia", "Saint-Petersburg",
-      "Hotel Leopold", "Marocco", "Ourika", "Rokoko Hotel","Germany", "Berlin", "Hotel Rehberge Berlin Mitte");
-});
+test(
+  '"Hotel" query returns ["Russia", "Saint-Petersburg, "Hotel Leopold", "Marocco", "Ourika", "Rokoko Hotel", ' +
+    '"Germany", "Berlin", "Hotel Rehberge Berlin Mitte"]',
+  () => {
+    expect(findDataByQuery('Hotel')).toContain(
+      'Russia',
+      'Saint-Petersburg',
+      'Hotel Leopold',
+      'Marocco',
+      'Ourika',
+      'Rokoko Hotel',
+      'Germany',
+      'Berlin',
+      'Hotel Rehberge Berlin Mitte',
+    );
+  },
+);
 
 test('When entering a request for which there is no data, empty array is returned', () => {
   expect(findDataByQuery('Hello!')).toHaveLength(0);
