e2d9aae7c9b928e55478407f12ec3d09ad712af0
dxarch <emilepatch@gmail.com>
2023-07-25 19:49:56 +0300

    Finish lesson-8

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,13 @@
-import User from "../entities/User.js";
+import User from '../entities/User.js';
 test('Student constructor throws an error if required fields are missing', () => {
-    expect(() => new User({firstName: 'James'})).toThrow('Required properties are missing!');
+  expect(() => new User({ firstName: 'James' })).toThrow(
+    'Required properties are missing!',
+  );
 });
 
 it('Returns correct full name', () => {
-    const user = new User({firstName: 'James', lastName: 'Smith'});
-    const fullName = 'James Smith';
+  const user = new User({ firstName: 'James', lastName: 'Smith' });
+  const fullName = 'James Smith';
 
-    expect(user.fullName).toEqual(fullName);
-})
+  expect(user.fullName).toEqual(fullName);
+});
