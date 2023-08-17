2eb722b4c59baf21b0f8039d231977527896c627
dxarch <emilepatch@gmail.com>
2023-08-18 00:35:08 +0300

    Finish file task

--- src/js/form.js
+++ src/js/form.js
@@ -1,5 +1,33 @@
 const form = document.forms.form;
 const fileInput = form.elements.file;
+const formImage = document.querySelector('.form__image-container');
+
+fileInput.addEventListener('change', async (e) => {
+  const file = fileInput.files[0];
+
+  if (file.type.startsWith('image/')) {
+    const reader = new FileReader();
+    const promise = new Promise((resolve) => {
+      reader.addEventListener('load', (e) => {
+        resolve(e.target.result);
+      });
+    });
+
+    reader.readAsDataURL(file);
+
+    const readResult = await promise;
+    formImage.innerHTML = `<img class='form__image' src=${readResult} alt='user image'>`;
+  } else {
+    formImage.innerHTML = `File is not an image: ${file.name}`;
+    fileInput.value = '';
+  }
+
+});
+
+formImage.addEventListener('click', (e) => {
+  fileInput.click();
+});
+
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
 
