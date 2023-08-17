0a52c85926c54dd82148b9eed77729f7c3f171b9
dxarch <emilepatch@gmail.com>
2023-08-17 21:03:07 +0300

    Add file submit to server

---
+++ src/js/form.js
@@ -0,0 +1,17 @@
+const form = document.forms.form;
+const fileInput = form.elements.file;
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    if (fileInput.files.length > 0) {
+
+        const formData = new FormData(form);
+        const fetchOptions = {
+            method: 'POST',
+            body: formData,
+        };
+
+        const result = await fetch('https://if-student-api.onrender.com/api/file', fetchOptions);
+        console.log(result);
+    }
+});
