796aa4247ec75baab1109f290edfe12900d593d6
dxarch <emilepatch@gmail.com>
2023-08-21 20:38:29 +0300

    Refactor js code

--- src/js/stack.js
+++ src/js/stack.js
@@ -1,17 +1,17 @@
 const checkObjectBraces = (objAsString) => {
-    const stack = [];
+  const stack = [];
 
-    for (let i = 0; i < objAsString.length; i++) {
-        if (objAsString[i] === '{'){
-            stack.push(objAsString[i]);
-        } else if (objAsString[i] === '}') {
-            if (!stack.pop()) {
-                return false;
-            }
-        }
+  for (let i = 0; i < objAsString.length; i++) {
+    if (objAsString[i] === '{') {
+      stack.push(objAsString[i]);
+    } else if (objAsString[i] === '}') {
+      if (!stack.pop()) {
+        return false;
+      }
     }
+  }
 
-    return !stack.length;
+  return !stack.length;
 };
 
 const objString = '{user: {name: }{"John", age: 21{}}';
